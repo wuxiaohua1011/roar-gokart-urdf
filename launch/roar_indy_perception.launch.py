03c23836d962fc1ccc6e67c6f1e2953132e9c740
Michael Wu <wuxiaohua1011@berkeley.edu>
2022-04-06 23:58:50 -0700

    launch files modified, will work on a new urdf tmr

--- launch/roar_indy_perception.launch.py
+++ launch/roar_indy_perception.launch.py
@@ -10,24 +10,8 @@ def generate_launch_description():
     pkg_share = launch_ros.substitutions.FindPackageShare(package="indy_bot_urdf").find(
         "indy_bot_urdf"
     )
-    default_model_path = os.path.join(
-        pkg_share, "src/description/indy_bot_description.urdf"
-    )
-    default_rviz_config_path = os.path.join(pkg_share, "rviz/urdf_config.rviz")
+    default_rviz_config_path = os.path.join(pkg_share, "rviz/with_sensor_config.rviz")
 
-    robot_state_publisher_node = launch_ros.actions.Node(
-        package="robot_state_publisher",
-        executable="robot_state_publisher",
-        parameters=[
-            {"robot_description": Command(["xacro ", LaunchConfiguration("model")])}
-        ],
-    )
-    joint_state_publisher_node = launch_ros.actions.Node(
-        package="joint_state_publisher",
-        executable="joint_state_publisher",
-        name="joint_state_publisher",
-        condition=launch.conditions.UnlessCondition(LaunchConfiguration("gui")),
-    )
     rviz_node = launch_ros.actions.Node(
         package="rviz2",
         executable="rviz2",
@@ -36,17 +20,44 @@ def generate_launch_description():
         arguments=["-d", LaunchConfiguration("rvizconfig")],
     )
 
+    robot_state_node = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("indy_bot_urdf"),
+                "launch",
+                "state_publisher_no_rviz.launch.py",
+            )
+        )
+    )
+    livox_node = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("livox_ros2_driver"),
+                "launch/indy_livox_lidar.launch.py",
+            )
+        ),
+        launch_arguments={
+            "lidar_publish_freq": launch.substitutions.LaunchConfiguration(
+                "lidar_publish_freq"
+            ),
+            "frame_id": launch.substitutions.LaunchConfiguration("frame_id"),
+        }.items(),
+    )
+    zed_node = launch.actions.IncludeLaunchDescription(
+        launch.launch_description_sources.PythonLaunchDescriptionSource(
+            os.path.join(
+                get_package_share_directory("zed_wrapper"),
+                "launch",
+                "indy_zed2i_no_rviz.launch.py",
+            )
+        )
+    )
     return launch.LaunchDescription(
         [
             launch.actions.DeclareLaunchArgument(
                 name="gui",
                 default_value="True",
                 description="Flag to enable joint_state_publisher_gui",
-            ),
-            launch.actions.DeclareLaunchArgument(
-                name="model",
-                default_value=default_model_path,
-                description="Absolute path to robot urdf file",
             ),
             launch.actions.DeclareLaunchArgument(
                 name="rvizconfig",
@@ -61,33 +72,9 @@ def generate_launch_description():
                 name="frame_id",
                 default_value="lidar_link",
             ),
-            launch.actions.IncludeLaunchDescription(
-                launch.launch_description_sources.PythonLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory("livox_ros2_driver"),
-                        "launch/indy_livox_lidar.launch.py",
-                    )
-                ),
-                launch_arguments={
-                    "lidar_publish_freq": launch.substitutions.LaunchConfiguration(
-                        "lidar_publish_freq"
-                    ),
-                    "frame_id": launch.substitutions.LaunchConfiguration("frame_id"),
-                }.items(),
-            ),
-            launch.actions.IncludeLaunchDescription(
-                launch.launch_description_sources.PythonLaunchDescriptionSource(
-                    os.path.join(
-                        get_package_share_directory("avt_vimba_camera"),
-                        "launch/michael.launch.py",
-                    )
-                ),
-                launch_arguments={
-                    "default_camera_param_file": "/home/roar/Desktop/projects/roar_ros/src/avt_vimba_camera/config/main/default.yaml"
-                }.items(),
-            ),
-            joint_state_publisher_node,
-            robot_state_publisher_node,
+            livox_node,
+            zed_node,
+            robot_state_node,
             rviz_node,
         ]
     )
